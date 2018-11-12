fd8c1b23119a3badee0bbe60189dfa113443b088
saviokay <saviosebastian08@gmail.com>
2018-11-11 23:56:06 -0500

    Initial Commit With Drop Commands.

---
+++ ddl.sql
@@ -0,0 +1,12 @@
+--Drop Commands To Avoid Redundancies.
+
+drop table message;
+drop table Review;
+drop table Payout_Info;
+drop table Booking_Info;
+drop table Available_Table;
+drop table Listing;
+drop table Guest;
+drop table Host;
+drop table Users;
+
