66fcc0208eeb6f992552bbfa872b9128616de38b
saviokay <saviosebastian08@gmail.com>
2018-11-11 23:59:59 -0500

    Add Initial Create Commands.

--- ddl.sql
+++ ddl.sql
@@ -10,3 +10,29 @@ drop table Guest;
 drop table Host;
 drop table Users;
 
+--Create Commands For Datasbase Schema.
+
+CREATE Or Replace TYPE aminitiesType AS VARRAY(20) OF VARCHAR(50);
+
+create table users
+  (user_id int,
+   name char(50),
+   address varchar(100),
+   phone_number varchar(20),
+   email varchar(50),
+   password varchar(20),
+   user_type char(5),
+   primary key(user_id));
+
+create table host
+     (host_id int,
+      user_id int,
+      name char(50),
+      address varchar(100),
+      phone_number varchar(20),
+      email varchar(50),
+      password varchar(20),
+      avg_rating number(3,2),
+      account_number varchar(30),
+      primary key (host_id),
+      foreign key (user_id) references users (user_id));
